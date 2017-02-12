5469f5ebd7155832c6b8110298e7b9cb52b26ad4
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-02-12 21:43:10 +0100

    Bulkl path fix reverse

---
+++ Models/ProjectMapper.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.1
+ *
+ * @category   TBD
+ * @package    TBD
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @copyright  Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://orange-management.com
+ */
+namespace Modules\ProjectManagement\Models;
+
+use Modules\Calendar\Models\CalendarMapper;
+use phpOMS\DataStorage\Database\DataMapperAbstract;
+use phpOMS\DataStorage\Database\Query\Builder;
+use phpOMS\DataStorage\Database\Query\Column;
+use phpOMS\DataStorage\Database\RelationType;
+use Modules\Tasks\Models\TaskMapper;
+
+/**
+ * Mapper class.
+ *
+ * @category   Calendar
+ * @package    Modules
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @license    OMS License 1.0
+ * @link       http://orange-management.com
+ * @since      1.0.0
+ */
+class ProjectMapper extends DataMapperAbstract
+{
+
+    /**
+     * Columns.
+     *
+     * @var array
+     * @since 1.0.0
+     */
+    protected static $columns = [
+        'projectmanagement_project_id'          => ['name' => 'projectmanagement_project_id', 'type' => 'int', 'internal' => 'id'],
+        'projectmanagement_project_name'        => ['name' => 'projectmanagement_project_name', 'type' => 'string', 'internal' => 'name'],
+        'projectmanagement_project_description' => ['name' => 'projectmanagement_project_description', 'type' => 'string', 'internal' => 'description'],
+        'projectmanagement_project_calendar'    => ['name' => 'projectmanagement_project_calendar', 'type' => 'int', 'internal' => 'calendar'],
+        'projectmanagement_project_costs'       => ['name' => 'projectmanagement_project_costs', 'type' => 'Serializable', 'internal' => 'costs'],
+        'projectmanagement_project_budget'      => ['name' => 'projectmanagement_project_budget', 'type' => 'Serializable', 'internal' => 'budget'],
+        'projectmanagement_project_earnings'    => ['name' => 'projectmanagement_project_earnings', 'type' => 'Serializable', 'internal' => 'earnings'],
+        'projectmanagement_project_start'       => ['name' => 'projectmanagement_project_start', 'type' => 'DateTime', 'internal' => 'start'],
+        'projectmanagement_project_end'         => ['name' => 'projectmanagement_project_end', 'type' => 'DateTime', 'internal' => 'end'],
+        'projectmanagement_project_created_by' => ['name' => 'projectmanagement_project_created_by', 'type' => 'int', 'internal' => 'createdBy'],
+        'projectmanagement_project_created_at'  => ['name' => 'projectmanagement_project_created_at', 'type' => 'DateTime', 'internal' => 'createdAt'],
+    ];
+
+    /**
+     * Has many relation.
+     *
+     * @var array
+     * @since 1.0.0
+     */
+    protected static $hasMany = [
+        'tasks' => [
+            'mapper'         => TaskMapper::class,
+            'table'          => 'projectmanagement_task_relation',
+            'dst'            => 'projectmanagement_task_relation_dst',
+            'src'            => 'projectmanagement_task_relation_src',
+        ],
+    ];
+
+    /**
+     * Has one relation.
+     *
+     * @var array
+     * @since 1.0.0
+     */
+    protected static $ownsOne = [
+        'calendar' => [
+            'mapper' => CalendarMapper::class,
+            'src'    => 'projectmanagement_project_calendar',
+        ],
+    ];
+
+    /**
+     * Primary table.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    protected static $table = 'projectmanagement_project';
+
+    /**
+     * Created at.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    protected static $createdAt = 'projectmanagement_project_created_at';
+
+    /**
+     * Primary field name.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    protected static $primaryField = 'projectmanagement_project_id';
+
+    /**
+     * Create object.
+     *
+     * @param mixed $obj       Object
+     * @param int   $relations Behavior for relations creation
+     *
+     * @return mixed
+     *
+     * @since  1.0.0
+     * @author Dennis Eichhorn <d.eichhorn@oms.com>
+     */
+    public static function create($obj, int $relations = RelationType::ALL)
+    {
+        try {
+            $objId = parent::create($obj, $relations);
+            $query = new Builder(self::$db);
+            $query->prefix(self::$db->getPrefix())
+                ->insert(
+                    'account_permission_account',
+                    'account_permission_from',
+                    'account_permission_for',
+                    'account_permission_id1',
+                    'account_permission_id2',
+                    'account_permission_r',
+                    'account_permission_w',
+                    'account_permission_m',
+                    'account_permission_d',
+                    'account_permission_p'
+                )
+                ->into('account_permission')
+                ->values($obj->getCreatedBy(), 'calendar_project', 'calendar_project', 1, $objId, 1, 1, 1, 1, 1);
+
+            self::$db->con->prepare($query->toSql())->execute();
+        } catch (\Exception $e) {
+            var_dump($e->getMessage());
+
+            return false;
+        }
+
+        return $objId;
+    }
+
+    /**
+     * Find.
+     *
+     * @param array $columns Columns to select
+     *
+     * @return Builder
+     *
+     * @since  1.0.0
+     * @author Dennis Eichhorn <d.eichhorn@oms.com>
+     */
+    public static function find(...$columns) : Builder
+    {
+        return parent::find(...$columns)->from('account_permission')
+            ->where('account_permission.account_permission_for', '=', 'calendar_project')
+            ->where('account_permission.account_permission_id1', '=', 1)
+            ->where('calendar_project.calendar_project_id', '=', new Column('account_permission.account_permission_id2'))
+            ->where('account_permission.account_permission_r', '=', 1);
+    }
+}
---
+++ Models/ProjectStatus.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.1
+ *
+ * @category   TBD
+ * @package    TBD
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @copyright  Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://orange-management.com
+ */
+namespace Modules\ProjectManagement\Models;
+
+use phpOMS\Datatypes\Enum;
+
+/**
+ * Task type enum.
+ *
+ * @category   Tasks
+ * @package    Modules
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @license    OMS License 1.0
+ * @link       http://orange-management.com
+ * @since      1.0.0
+ */
+abstract class ProjectStatus extends Enum
+{
+    /* public */ const ACTIVE   = 0;
+    /* public */ const INACTIVE = 1;
+    /* public */ const HOLD     = 2;
+    /* public */ const CANCELED = 3;
+    /* public */ const FINISHED = 4;
+}
