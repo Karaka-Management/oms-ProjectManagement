7c20fd0fe8d8587a6c67612a04d7863d8de9dd18
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-09-10 20:40:12 +0200

    fix typehint

--- Models/Project.php
+++ Models/Project.php
@@ -695,11 +695,11 @@ class Project
     /**
      * Get created at
      *
-     * @return \DateTime
+     * @return \DateTimeImmutable
      *
      * @since 1.0.0
      */
-    public function getCreatedAt() : \DateTimeInterface
+    public function getCreatedAt() : \DateTimeImmutable
     {
         return $this->createdAt;
     }
