cb059e73e6291b6693bb53d5db8f3b6c3c03166b
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-10-24 17:52:24 +0200

    cs and phpstan fixes

--- Models/ProjectMapper.php
+++ Models/ProjectMapper.php
@@ -79,7 +79,7 @@ final class ProjectMapper extends DataMapperAbstract
     /**
      * Has one relation.
      *
-     * @var array<string, array{mapper:string, self:string, by?:string, column?:string}>
+     * @var array<string, array{mapper:string, external:string, by?:string, column?:string, conditional?:bool}>
      * @since 1.0.0
      */
     protected static array $ownsOne = [
@@ -92,7 +92,7 @@ final class ProjectMapper extends DataMapperAbstract
     /**
      * Belongs to.
      *
-     * @var array<string, array{mapper:string, self:string}>
+     * @var array<string, array{mapper:string, external:string}>
      * @since 1.0.0
      */
     protected static array $belongsTo = [
