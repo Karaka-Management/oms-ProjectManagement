2d01b73ead6fffaf1edc9b33f3c0262f166f649c
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-05-28 12:19:04 +0000

    phpcs autofixes

--- Models/ProjectAttributeValue.php
+++ Models/ProjectAttributeValue.php
@@ -15,7 +15,6 @@ declare(strict_types=1);
 namespace Modules\ProjectManagement\Models;
 
 use phpOMS\Localization\BaseStringL11n;
-use phpOMS\Localization\ISO3166TwoEnum;
 use phpOMS\Localization\ISO639x1Enum;
 
 /**
