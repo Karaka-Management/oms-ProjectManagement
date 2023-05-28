bfec4ffbf7997a89df6c3e219c49fb498abd0d79
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-05-28 01:13:23 +0000

    style fixes, bug fixes

--- Theme/Backend/projectmanagement-list.tpl.php
+++ Theme/Backend/projectmanagement-list.tpl.php
@@ -24,7 +24,7 @@ echo $this->getData('nav')->render(); ?>
 <div class="row">
     <div class="col-xs-12">
         <div class="portlet">
-            <div class="portlet-head"><?= $this->getHtml('Projects'); ?><i class="fa fa-download floatRight download btn"></i></div>
+            <div class="portlet-head"><?= $this->getHtml('Projects'); ?><i class="lni lni-download download btn end-xs"></i></div>
             <div class="slider">
             <table class="default sticky">
                 <thead>
